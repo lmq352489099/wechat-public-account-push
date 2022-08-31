10e9076aa57c73656731ffca87fedbff15b81f0e
lmq352489099 <37898934+lmq352489099@users.noreply.github.com>
2022-08-31 14:52:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,12 +27,12 @@ export const config = {
     USERS: [
     {
       name: "林暖暖",
-      id: "linnuannuan522"
+      id: "oHBuO6bk21bf2iY7vDAMo5R5aZ2w"
     } 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "lmq4088"}, 
+      {name: '自己',id: "oHBuO6bk21bf2iY7vDAMo5R5aZ2w"}, 
     ],
      
     /**
