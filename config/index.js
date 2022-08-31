793b84415d0ef75a703c6effb148a33e706f7fa8
lmq352489099 <37898934+lmq352489099@users.noreply.github.com>
2022-08-31 14:40:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     TEMPLATE_ID: "7TLvu_Bbg7KSvik4AiFcX8dWgdLCba8XMDbVVZ8a5xA",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    CALLBACK_TEMPLATE_ID: "7TLvu_Bbg7KSvik4AiFcX8dWgdLCba8XMDbVVZ8a5xA",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
